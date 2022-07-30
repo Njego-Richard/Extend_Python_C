301f6e66b1fdb785cb984d44ca10d84f1b7fb703
NJEGO RICHARD <njegorich@gmail.com>
2022-07-30 12:13:00 +0300

    update variables

--- c_extension_python.py
+++ c_extension_python.py
@@ -2,21 +2,21 @@ import c_module
 from time import time
 
 # Python fib version using recursion in python
-def py_fib(n):
-    if (n <= 1):
-        return n
-    return py_fib(n-1) + py_fib(n-2)
+def py_fib(x):
+    if (x <= 1):
+        return x
+    return py_fib(x-1) + py_fib(x-2)
 
-n = 5
+x = 5
 
 # C test
 t = time()
-c_res = c_module.c_fib(n)
+c_res = c_module.c_fib(x)
 c_time = time() - t
 
 # Python test
 t = time()
-py_res = py_fib(n)
+py_res = py_fib(x)
 py_time = time() - t
 
-print(f'Input: {n}\n{py_res=}, {py_time=}\n{c_res=}, {c_time=}')  
+print(f'Input: {x}\n{py_res=}, {py_time=}\n{c_res=}, {c_time=}')  
